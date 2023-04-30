dc24f043bc8e953ac4826ba660af0588f3249391
Aleksey_Filkin <acid_wind@mail.ru>
2023-04-30 10:29:24 +0700

    Task3 change solve

--- Task3.py
+++ Task3.py
@@ -1,7 +1,7 @@
 """Задача 14: Требуется вывести все целые степени двойки (т.е. числа вида 2k), не превосходящие числа N."""
 
-number = int(input('Введите число-предел: '))
-final_number = 2
-while final_number < number:
-    print(final_number, end=' ')
-    final_number = final_number * 2
+n = int(input("Введите число N: ")) 
+i = 0 
+while 2 ** i <= n:     
+    print(2 ** i)     
+    i += 1
